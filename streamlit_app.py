47d5660271888f1877153fa94cb344e5d188f9f9
Ahalya Sakthivelu <115523634+Ahalya-V-S@users.noreply.github.com>
2024-07-29 10:50:07 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,12 @@ def preprocess_image(image):
 # Streamlit App
 st.title('Fashion Item Classifier')
 
+
+# Set a header
+st.header("This is a Header")
+
+# Set a subheader
+st.subheader("This is a Subheader")
 uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
