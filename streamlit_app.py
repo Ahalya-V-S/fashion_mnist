ad9a1737f299f1bc6843d4e7131b4e4eb9987da7
Ahalya Sakthivelu <115523634+Ahalya-V-S@users.noreply.github.com>
2024-07-29 09:29:12 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 import os
 
-model_path = "/workspaces/fashion_mnist/trained_fashion_mnist_model.h5"
+model_path = "/workspaces/fashion_mnist/trained_fashion_mnist.h5"
 # Load the pre-trained model
 model = tf.keras.models.load_model(model_path)
 
